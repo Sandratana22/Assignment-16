2caa109d7e441f911f7d3cdf6ae46e3ddd9ff371
Sandratana22 <andrianjoharys06@gmail.com>
2024-10-07 09:43:47 -0700

    Create grade.hpp

---
+++ grade.hpp
@@ -0,0 +1,24 @@
+#ifndef GRADE_HPP
+#define GRADE_HPP
+
+namespace average {
+    double average(int grade[], int size);
+}
+
+namespace max {
+    int max(int grade[], int size); 
+}
+
+namespace min {
+    int min(int grade[], int size); 
+}
+
+namespace aboveAverage {
+    void aboveAverage(int grade[], int size, double average); 
+}
+
+namespace compareGrade {
+    void compareGrade(int grade[], int size);
+}
+
+#endif
